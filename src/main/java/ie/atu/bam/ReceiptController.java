364c1d9bf1b230a0efb40cc1e98de3b7673e33c4
Mark <G00419267@atu.ie>
2025-01-06 02:23:34 +0000

    Receipts for transfer and deposit/withdraw added and working

--- src/main/java/ie/atu/bam/ReceiptController.java
+++ src/main/java/ie/atu/bam/ReceiptController.java
@@ -1,15 +1,38 @@
 package ie.atu.bam;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/customer")
+import java.util.List;
+
+@RequestMapping("/receipt")
 @RestController
 public class ReceiptController {
     private final ReceiptService receiptService;
 
     public ReceiptController(ReceiptService receiptService) {
         this.receiptService = receiptService;
+    }
+
+    @GetMapping("/getAll")
+    public List<Receipt> get(){
+        return receiptService.getAll();
+    }
+
+    @PostMapping("/createRec/{IBAN}/{uID}/{inout}/{num}")
+    public void createReceipt(@PathVariable int IBAN,@PathVariable Long uID,@PathVariable String inout,@PathVariable float num){
+        receiptService.withDepRec(IBAN, uID, inout, num);
+    }
+
+    @GetMapping("/getRec/{uID}")
+    public List<Object> getRecs(@PathVariable Long uID){
+        return receiptService.returnRec(uID);
+    }
+
+    @PostMapping("/transferRec/{IBAN1}/{uID1}/{IBAN2}/{uID2}/{num}")
+    public void transferRec(@PathVariable int IBAN1,@PathVariable Long uID1, @PathVariable int IBAN2,@PathVariable Long uID2,@PathVariable float num){
+        receiptService.transferRec(IBAN1, uID1, IBAN2, uID2, num);
     }
 
 
